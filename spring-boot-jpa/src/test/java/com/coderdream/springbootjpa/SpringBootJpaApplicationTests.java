6a2322d28cf9983904315680d931f1b9aaf5828c
xulin28709 <xulin28709@billjc.com>
2021-04-20 14:51:20 +0800

    增加jpa初始项目

---
+++ spring-boot-jpa/src/test/java/com/coderdream/springbootjpa/SpringBootJpaApplicationTests.java
@@ -0,0 +1,20 @@
+package com.coderdream.springbootjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	void testDemo(){
+		System.out.println("testing");
+
+		com.mysql.cj.jdbc.Driver a = null;
+	}
+
+}
